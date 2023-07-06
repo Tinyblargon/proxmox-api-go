51687168b9a8358258ed4baea2fb01ca79f479e8
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-07-06 21:10:06 +0000

    refactor: Pools cli test

--- test/cli/Pools/list_pools_test.go
+++ test/cli/Pools/list_pools_test.go
@@ -8,128 +8,90 @@ import (
 
 // Test0
 func Test_Pool_0_Cleanup(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   true,
-		Args:     []string{"-i", "delete", "pool", "test-pool0"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i delete pool test-pool0", cliTest.Variables())
+	test.Error()
 }
 
 func Test_Pool_0_Create_Without_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   false,
-		Args:     []string{"-i", "create", "pool", "test-pool0"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i create pool test-pool0", cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool0)")
 }
 
 func Test_Pool_0_List(t *testing.T) {
-	Test := cliTest.Test{
-		Contains: []string{`"test-pool0"`},
-		Args:     []string{"-i", "list", "pools"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i list pools", cliTest.Variables())
+	test.NoError()
+	test.Contains(`"test-pool0"`)
 }
 
 func Test_Pool_0_Get_Without_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		NotContains: []string{`"comment"`},
-		Args:        []string{"-i", "get", "pool", "test-pool0"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i get pool test-pool0", cliTest.Variables())
+	test.NoError()
+	test.NotContains(`"comment"`)
 }
 
 func Test_Pool_0_Update_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Contains: []string{"(test-pool0)"},
-		Args:     []string{"-i", "update", "poolcomment", "test-pool0", "this is a comment"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, `-i update poolcomment test-pool0 "this is a comment"`, cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool0)")
 }
 
 func Test_Pool_0_Get_With_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Contains: []string{`"this is a comment"`},
-		Args:     []string{"-i", "get", "pool", "test-pool0"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i get pool test-pool0", cliTest.Variables())
+	test.NoError()
+	test.Contains(`"this is a comment"`)
 }
 
 func Test_Pool_0_Delete(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   false,
-		Args:     []string{"-i", "delete", "pool", "test-pool0"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i delete pool test-pool0", cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool0)")
 }
 
 func Test_Pool_0_Removed(t *testing.T) {
-	Test := cliTest.Test{
-		NotContains: []string{`"test-pool0"`},
-		Args:        []string{"-i", "list", "pools"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i list pools", cliTest.Variables())
+	test.NoError()
+	test.NotContains(`"test-pool0"`)
 }
 
 // Test1
 func Test_Pool_1_Cleanup(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   true,
-		Args:     []string{"-i", "delete", "pool", "test-pool1"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i delete pool test-pool1", cliTest.Variables())
+	test.Error()
 }
 
 func Test_Pool_1_Create_With_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   false,
-		Args:     []string{"-i", "create", "pool", "test-pool1", "This is a comment"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, `-i create pool test-pool1 "This is a comment"`, cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool1)")
 }
 
 func Test_Pool_1_Get_With_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Contains: []string{`"This is a comment"`},
-		Args:     []string{"-i", "get", "pool", "test-pool1"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i get pool test-pool1", cliTest.Variables())
+	test.NoError()
+	test.Contains(`"This is a comment"`)
 }
 
 func Test_Pool_1_Update_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		Contains: []string{"(test-pool1)"},
-		Args:     []string{"-i", "update", "poolcomment", "test-pool1"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i update poolcomment test-pool1", cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool1)")
 }
 
 func Test_Pool_1_Get_Without_Comment(t *testing.T) {
-	Test := cliTest.Test{
-		NotContains: []string{`"comment"`},
-		Args:        []string{"-i", "get", "pool", "test-pool1"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i get pool test-pool1", cliTest.Variables())
+	test.NoError()
+	test.NotContains(`"comment"`)
 }
 
 func Test_Pool_1_Delete(t *testing.T) {
-	Test := cliTest.Test{
-		Expected: "",
-		ReqErr:   false,
-		Args:     []string{"-i", "delete", "pool", "test-pool1"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i delete pool test-pool1", cliTest.Variables())
+	test.NoError()
+	test.Contains("(test-pool1)")
 }
 
 func Test_Pool_1_Removed(t *testing.T) {
-	Test := cliTest.Test{
-		NotContains: []string{`"test-pool1"`},
-		Args:        []string{"-i", "list", "pools"},
-	}
-	Test.StandardTest(t)
+	test := cliTest.Run(t, "-i list pools", cliTest.Variables())
+	test.NoError()
+	test.NotContains(`"test-pool1"`)
 }
