39346751712f4c3b22d2767a94297d5093ec762b
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-07-08 07:52:38 +0000

    feat: create new `QemuCpu` schema
    this new schema will implement all cpu options provided by proxmox

---
+++ proxmox/config_qemu_cpu.go
@@ -0,0 +1,46 @@
+package proxmox
+
+type CpuFlags struct {
+	AES        *bool `json:"aes,omitempty"`        // Activate AES instruction set for HW acceleration.
+	AmdNoSSB   *bool `json:"amdnossb,omitempty"`   // Notifies guest OS that host is not vulnerable for Spectre on AMD CPUs.
+	AmdSSBD    *bool `json:"amdssbd,omitempty"`    // Improves Spectre mitigation performance with AMD CPUs, best used with "VirtSSBD".
+	HvEvmcs    *bool `json:"hvevmcs,omitempty"`    // Improve performance for nested virtualization. Only supported on Intel CPUs.
+	HvTlbFlush *bool `json:"hvtlbflush,omitempty"` // Improve performance in overcommitted Windows guests. May lead to guest bluescreens on old CPUs.
+	Ibpb       *bool `json:"ibpb,omitempty"`       // Allows improved Spectre mitigation with AMD CPUs.
+	MdClear    *bool `json:"mdclear,omitempty"`    // Required to let the guest OS know if MDS is mitigated correctly.
+	PCID       *bool `json:"pcid,omitempty"`       // Meltdown fix cost reduction on Westmere, Sandy-, and IvyBridge Intel CPUs.
+	Pdpe1GB    *bool `json:"pdpe1gb,omitempty"`    // Allow guest OS to use 1GB size pages, if host HW supports it.
+	SpecCtrl   *bool `json:"specctrl,omitempty"`   // Allows improved Spectre mitigation with Intel CPUs.
+	SSBD       *bool `json:"ssbd,omitempty"`       // Protection for "Speculative Store Bypass" for Intel models.
+	VirtSSBD   *bool `json:"cirtssbd,omitempty"`   // Basis for "Speculative Store Bypass" protection for AMD models.
+}
+
+// min value 0 is unlimited, max value of 128
+type CpuLimit uint8
+
+// min value 1, max value 4
+type CpuSockets uint8
+
+// enum
+type CpuType string
+
+// min value 0 is unset, max value of 262144
+type CpuUnits uint32
+
+// min value 0 is unset, max value 512. is QemuCpuCores * CpuSockets
+type CpuVirtualCores uint16
+
+type QemuCPU struct {
+	Affinity     []uint          `json:"affinity,omitempty"`
+	Cores        QemuCpuCores    `json:"cores"`
+	Flags        CpuFlags        `json:"flags"`
+	Limit        CpuLimit        `json:"limit,omitempty"`
+	Numa         bool            `json:"numa"`
+	Sockets      CpuSockets      `json:"sockets"`
+	Type         CpuType         `json:"type,omitempty"`
+	Units        CpuUnits        `json:"units,omitempty"`
+	VirtualCores CpuVirtualCores `json:"vcores,omitempty"`
+}
+
+// min value 1, max value of 128
+type QemuCpuCores uint8
